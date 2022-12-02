0f537af8df68b41befd94ea6ba4e40d05cc7cb06
Pr3vYCa <wendy.prasna@gmail.com>
2022-12-02 11:45:37 +0700

    change responsive

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -31,7 +31,7 @@ function Favorites() {
         <>
             <Navbar />
             <div className={`flex flex-col pl-[10%] pr-[10%] pt-5 pb-5 bg-gradient-to-b min-h-screen ${toggle ? `from-indigo-500 via-purple-500 to-pink-500 ` : `from-sky-500 via-teal-500 to-green-500`}`}>
-                <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6'>
+                <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-6'>
                     {
                         favorite.map((item) => {
                             return (
